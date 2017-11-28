be98817bedd093d9afccc77de5003c13462bed4b
Dan Nollette <nollette.dan@gmail.com>
2017-11-28 08:26:55 -0800

    added scope

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,24 @@ class Product < ActiveRecord::Base
   has_many :reviews
   validates :name, :cost, :country, :presence => true
 
-  scope :today, -> { where("created_at >=?", Time.now.beginning_of_day)}
+  scope :most_reviews, -> {(
+    select("products.*, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(1)
+  )}
+
+  scope :recently_added, -> {(
+    select("products.*")
+    .group("products.id")
+    .order("created_at DESC")
+    .limit(3)
+  )}
+
+  scope :highest_cost_first, -> {(
+    select("products.*")
+    .group("products.id")
+    .order("cost DESC")
+  )}
 end
